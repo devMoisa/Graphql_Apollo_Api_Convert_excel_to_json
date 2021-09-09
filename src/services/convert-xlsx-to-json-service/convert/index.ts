d2c95f9d28058c5d172f42b0615a8d33a973391f
Moisés Vilas Boas <moisesboas@gmail.com>
2021-09-08 21:44:00 -0300

    fix: indexs

--- src/services/convert-xlsx-to-json-service/convert/index.ts
+++ src/services/convert-xlsx-to-json-service/convert/index.ts
@@ -1,7 +1,4 @@
 let xlsx = require("xlsx");
-let path = require("path");
-// import fs from "fs";
-// import la from './../../../tmp/uploads/'
 
 export const convert = async (streamPipe: any, filename: any) => {
   let wb = xlsx.readFile(`./src/tmp/uploads/${filename}`);
@@ -10,7 +7,6 @@ export const convert = async (streamPipe: any, filename: any) => {
   let sheetValue = wb.Sheets[sheetName];
   let excelData = xlsx.utils.sheet_to_json(sheetValue);
 
-  // console.log("DEBUG!!!: \n", sheetValue);
   console.log(excelData);
   console.log("\n \n \n \n \n \n");
 };
